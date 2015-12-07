cd406ac5da911c7c1e22fc38f199f57a7b70d9c9
Luis Brosa <luisbg@gmail.com>
2015-12-07 12:30:48 +0000

    Se generan
    1) Listener para cuando se ejecuta el login
    2) Subscriber para cuando se quiera trabajar con subscriber dentro de UserBundle.
    
    3) Se crea una Nuevo controlador para Registration
    4) Se añade al composer Parse
    5) se modifica la plantilla de base_login, para tener un enlace para nuevos registros.
    
    6) Se añaden las traducciones personalizadas del FOSUserBundle

---
+++ src/Application/Sonata/UserBundle/EventListener/RegisterSub.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luisbg
+ * Date: 7/12/15
+ * Time: 11:14
+ */
+
+namespace Application\Sonata\UserBundle\EventListener;
+
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine; // for Symfony 2.1.0+
+
+class RegisterSub implements EventSubscriberInterface{
+
+
+    private $router;
+
+    /**
+     * Constructor
+     *
+     * @param router
+     */
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+        d($this->router);
+        exit();
+
+    }
+
+    public static function getSubscribedEvents()
+    {
+        //return the subscribed events, their methods and priorities
+        return array(
+
+        );
+
+    }
+
+    public function onRegisterDone(FilterUserResponseEvent $event)
+    {
+        d($event);
+        exit();
+        //$event->setResponse(new RedirectResponse($url));
+    }
+
+
+
+
+
+}
